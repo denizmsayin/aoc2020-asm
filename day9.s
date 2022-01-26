2117cecd2a4f09a41069b67e2da452cd809fde0f
DenizMS <denizmsayin@gmail.com>
2022-01-26 15:37:51 +0300

    day9 complete. that was relatively easy!

--- day9.s
+++ day9.s
@@ -2,12 +2,17 @@
 
     .extern getia
     .extern printi
+    .extern part2set
 
 .set PREAMBLE_LEN, 25
 
 fmt: .string "@\n"
 
 main:
+    xor %r12, %r12
+    call part2set
+    setc %r12b
+
     lea array(%rip), %rdi
     mov $1024, %rsi
     call getia
@@ -53,20 +58,67 @@ main:
     jl .Lloopi    
 
     # Fell down, could not find a match
-    push %rax
-    lea fmt(%rip), %rdi
-    call printi
-    add $8, %rsp
-    jmp .Lepi
+    
+    # Test if part1. If so, print and leave.
+    # Otherwise, move on to part2.
+    test %r12, %r12
+    je .Lprintresult
+    jmp .Lpart2
 
     inc %r14
 .Lck:
     cmp %r13, %r14
     jl .Lloop
     
-.Lepi:    
+.Lpart2:
+    # Alright... Now, %rax has the target value.
+    # Need to find a subsequence which sums to this
+    # thing. Since all values are positive; I 
+    # can grow/shrink based on the current sum.
+    
+    lea array(%rip), %rdi # i pointer
+    mov %rdi, %rsi # j pointer
+    xor %rdx, %rdx # sum of subsequence
+
+.Lgrow:
+    addq (%rsi), %rdx
+    add $8, %rsi
+    jmp .Ldecision
+.Lshrink:
+    subq (%rdi), %rdx
+    add $8, %rdi
+.Ldecision:
+    cmp %rax, %rdx # compare value with current sum
+    jl .Lgrow
+    jg .Lshrink
+
+    # Otherwise, equal. Subsequence found!
+    # Now just gotta find the min and max.
+    mov (%rdi), %r8 # min
+    mov %r8, %r9 # max
+    jmp .Lminmaxcheck
+
+.Lminmaxloop:
+    mov (%rdi), %r10
+    cmp %r8, %r10 # update if value < min
+    cmovl %r10, %r8
+    cmp %r9, %r10 # update if value > max
+    cmovg %r10, %r9
+.Lminmaxcheck:
+    add $8, %rdi
+    cmp %rdi, %rsi
+    jne .Lminmaxloop    
+
+    lea (%r8, %r9), %rax
+.Lprintresult:
+    push %rax
+    lea fmt(%rip), %rdi
+    call printi
+    add $8, %rsp
 
     ret
+
+dbgfmt: .string "min:@ max:@ sum:@\n"
 
     .data
 array: .fill 8192 
